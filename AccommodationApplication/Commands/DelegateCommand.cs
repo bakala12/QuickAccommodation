810092d6901a75951e3f2bcd9ac5cf136927e410
bakala12 <bakala12@o2.pl>
2015-10-26 00:07:59 +0100

    Adding DelegateCommand class

---
+++ AccommodationApplication/Commands/DelegateCommand.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using AccommodationApplication.Annotations;
+
+namespace AccommodationApplication.Commands
+{
+    public class DelegateCommand : ICommand
+    {
+        public DelegateCommand(Action<object> executeAction, Predicate<object> canExecutePredicate)
+        {
+            if (executeAction == null) throw new ArgumentNullException(nameof(executeAction), @"Action to execute cannot be null.");
+            if (canExecutePredicate == null) throw new ArgumentNullException(nameof(canExecutePredicate), @"Predicate CanExecute cannot be null.");
+            ExecuteAction = executeAction;
+            CanExecutePredicate = canExecutePredicate;
+        }
+
+        public DelegateCommand(Action<object> executeAction) : this(executeAction, x => true) { }
+
+        public Action<object> ExecuteAction { get; }
+
+        public Predicate<object> CanExecutePredicate { get; }
+
+        public bool CanExecute(object parameter)
+        {
+            return CanExecutePredicate(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            ExecuteAction(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        protected virtual void RaiseCanExecuteChangedEvent()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
