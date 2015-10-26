28c380ba75777dcd33612e5c084c1d9041fff247
bakala12 <bakala12@o2.pl>
2015-10-26 23:57:22 +0100

    Login window

--- AccommodationApplication/ViewModels/LoginWindowViewModel.cs
+++ AccommodationApplication/ViewModels/LoginWindowViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
@@ -10,11 +11,45 @@ namespace AccommodationApplication.ViewModels
 {
     public class LoginWindowViewModel : CloseableViewModel
     {
+        private string _login;
+        private string _password;
+
         public LoginWindowViewModel()
         {
             CloseCommand = new DelegateCommand(x=>Close());
+            LoginCommand = new DelegateCommand(x=>LogIn(), x=>CanLogIn());
         }
 
         public ICommand CloseCommand { get; set; }
+        public DelegateCommand LoginCommand { get; set; }
+
+        public string Login
+        {
+            get { return _login; }
+            set { _login = value; OnPropertyChanged(); }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set { _password = value; OnPropertyChanged(); }
+        }
+
+        protected virtual void LogIn()
+        {
+            Close();
+        }
+
+        protected virtual bool CanLogIn()
+        {
+            return !string.IsNullOrEmpty(Login) && !string.IsNullOrEmpty(Password);
+        }
+
+        protected override void OnPropertyChanged(string propertyName = null)
+        {
+            if(propertyName==null || propertyName.Equals(nameof(Login)) || propertyName.Equals(nameof(Password)))
+                LoginCommand.RaiseCanExecuteChangedEvent();
+            base.OnPropertyChanged(propertyName);
+        }
     }
 }
