2131d0939cc541f4a0ac24535a7f3ad45d6804f4
bakala12 <bakala12@o2.pl>
2015-11-02 11:43:17 +0100

    Begining of authentication system

---
+++ UserAuthorizationSystem/Identities/CustomIdentity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserAuthorizationSystem.Identities
+{
+    public class CustomIdentity : IIdentity
+    {
+        public CustomIdentity(string username, string email, string[] roles)
+        {
+            Username = username;
+            Email = email;
+            Roles = roles;
+        }
+
+        public string Username { get; private set; }
+        public string Email { get; private set; }
+        public string[] Roles { get; private set; }
+
+        #region IIdentity Members
+        public string AuthenticationType { get { return "Custom authentication"; } }
+
+        public bool IsAuthenticated { get { return !string.IsNullOrEmpty(Username); } }
+
+        public string Name { get { return Username; } }
+        #endregion
+    }
+}
