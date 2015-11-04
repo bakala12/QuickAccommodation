3d36aa935c8a418a3c6edc8e3cbc8c5259dc6100
bakala12 <bakala12@o2.pl>
2015-11-04 18:40:39 +0100

    Basic user data validation

---
+++ UserAuthorizationSystem/Validation/UserCredentialsValidator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AccommodationDataAccess.Domain;
+
+namespace UserAuthorizationSystem.Validation
+{
+    public class UserCredentialsValidator : BasicUserDataValidator, IUserCredentialsValidator
+    {
+        public bool ValidateUsername<T>(string username) where T:IUsersContext, IDisposable, new()
+        {
+            if (string.IsNullOrEmpty(username)) return false;
+            using (var context=new T())
+            {
+                return !context.Users.Any(u => u.Username == username);
+            }
+        }
+
+        public async Task<bool> ValidateUsernameAsync<T>(string username) where T : IUsersContext, IDisposable, new()
+        {
+            return await Task.Run(() => ValidateUsername<T>(username));
+        }
+
+        public virtual bool ValidatePassword(string password, out string reason)
+        {
+            reason=string.Empty;
+            if (string.IsNullOrEmpty(password))
+            {
+                reason = "Hasło nie może byc puste";
+                return false;
+            }
+            if (password.Length < 8)
+            {
+                reason = "Hasło musi zawierać przynajmniej 8 znaków";
+                return false;
+            }
+            if (password.Count(char.IsDigit) < 2)
+            {
+                reason = "Hasło musi zawierać przynajmniej dwie cyfry";
+                return false;
+            }
+            return true;
+        }
+
+        public virtual bool ValidatePasswordConfirmation(string password, string passwordConfirmed)
+        {
+            string reason;
+            return ValidatePassword(password, out reason) && ValidatePassword(passwordConfirmed, out reason) &&
+                   password.Equals(passwordConfirmed);
+        }
+    }
+}
