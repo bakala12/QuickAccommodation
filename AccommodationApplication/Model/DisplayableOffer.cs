5de2499aa60c31615eb63355eed55ac2e0217e0e
jablonskim <matjablonski94@gmail.com>
2015-11-14 18:07:03 +0100

    folder Model

---
+++ AccommodationApplication/Model/DisplayableOffer.cs
@@ -0,0 +1,31 @@
+﻿using AccommodationDataAccess.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccommodationApplication.Model
+{
+    public class DisplayableOffer
+    {
+        public DisplayableOffer(Offer offer)
+        {
+            OfferInfo offerInfo = offer.OfferInfo;
+            OfferStartTime = offerInfo.OfferStartTime;
+            OfferEndTime = offerInfo.OfferEndTime;
+            Address = offer.Place.Address;
+            AvailableVacanciesNumber = offerInfo.AvailableVacanciesNumber;
+            Price = offerInfo.Price;
+            Description = offerInfo.Description;
+            OfferPublishTime = offerInfo.OfferPublishTime;
+        }
+        public DateTime OfferStartTime { get; set; }
+        public DateTime OfferEndTime { get; set; }
+        public virtual Address Address { get; set; }
+        public int AvailableVacanciesNumber { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public DateTime OfferPublishTime { get; set; }
+    }
+}
