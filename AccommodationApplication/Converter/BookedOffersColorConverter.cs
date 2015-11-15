9ef74f16bfc8a9846918dc3180e91f222d0fc5d2
bakala12 <bakala12@o2.pl>
2015-11-15 22:36:41 +0100

    Booked offers on red background in myoffers

---
+++ AccommodationApplication/Converter/BookedOffersColorConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace AccommodationApplication.Converter
+{
+    public class BookedOffersColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool? b = value as bool?;
+            if (!b.HasValue || !b.Value) return null;
+            return new SolidColorBrush() {Color = Colors.Red};
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
