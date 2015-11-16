89898396ffa307eac0260fdbea8531c22f8cd885
jablonskim <matjablonski94@gmail.com>
2015-11-17 00:26:35 +0100

    testing nuggets

--- UnitTestProject/UnitTest1.cs
+++ UnitTestProject/UnitTest1.cs
@@ -1,5 +1,10 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Data.Entity;
+using AccommodationDataAccess.Domain;
+using AccommodationDataAccess.Model;
+using AccommodationApplication.ViewModels;
 
 namespace UnitTestProject
 {
@@ -7,8 +12,34 @@ namespace UnitTestProject
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Check_user_adding()
         {
+            //var mockSet = new Mock<DbSet<Offer>>();
+
+            //var mockContext = new Mock<AccommodationContext>();
+            //mockContext.Setup(m => m.Offers).Returns(mockSet.Object);
+
+            //var offerViewModel = new AddNewOfferViewModel();
+
+            //offerViewModel.AccommodationName = "nazwa";
+            //offerViewModel.AvailiableVacanciesNumber = "1";
+            //offerViewModel.City = "miasto";
+            //offerViewModel.Description = "opis";
+            //offerViewModel.EndDate = DateTime.Now;
+            //offerViewModel.StartDate = DateTime.Now;
+            //offerViewModel.LocalNumber = "1";
+            //offerViewModel.PostalCode = "11-111";
+            //offerViewModel.Price = "11";
+            //offerViewModel.Street = "11";
+
+            //offerViewModel.Add();
+
+            //using (var context = new AccommodationContext())
+            //{
+            //}
+
+
+
         }
     }
 }
