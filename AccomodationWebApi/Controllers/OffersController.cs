00beeeab29752388908f7d4ac11be7c680a3008d
jablonskim <matjablonski94@gmail.com>
2015-12-03 02:07:49 +0100

    test implementation of offers controller, it works :)

---
+++ AccomodationWebApi/Controllers/OffersController.cs
@@ -0,0 +1,48 @@
+﻿using AccommodationDataAccess.Domain;
+using AccommodationDataAccess.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Web.Http;
+
+namespace AccomodationWebApi.Controllers
+{
+    //test implementation only
+    public class OffersController : ApiController
+    {
+        public IEnumerable<Offer> GetAllOffers()
+        {
+            List<Offer> ret = new List<Offer>();
+
+            using (var context = new AccommodationContext())
+            {
+                context.Configuration.ProxyCreationEnabled = false;
+                ret = context.Offers.Take(20).ToList();
+                context.SaveChanges();
+            }
+
+            return ret;
+
+        }
+
+        public IHttpActionResult GetOffer(int id)
+        {
+            Offer offer = null;
+
+            using (var context = new AccommodationContext())
+            {
+                context.Configuration.ProxyCreationEnabled = false;
+                offer = context.Offers.FirstOrDefault(o => o.Id == id);
+            }
+
+            if (offer == null)
+            {
+                return NotFound();
+            }
+            return Ok(offer);
+        }
+    }
+}
--- AccomodationWebApi/Global.asax.cs
+++ AccomodationWebApi/Global.asax.cs
@@ -9,6 +9,7 @@ namespace AccomodationWebApi
 {
     public class WebApiApplication : System.Web.HttpApplication
     {
+
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
