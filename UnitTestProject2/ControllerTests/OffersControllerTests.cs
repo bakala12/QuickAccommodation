1e02391275d3cdeeefe3c4fa75815d72de3e0106
bakala12 <bakala12@o2.pl>
2015-12-22 00:12:46 +0100

    Test 2

--- UnitTestProject2/ControllerTests/OffersControllerTests.cs
+++ UnitTestProject2/ControllerTests/OffersControllerTests.cs
@@ -26,7 +26,7 @@ namespace UnitTestProject2.ControllerTests
         }
 
         [TestMethod]
-        public void TestGetOfferById()
+        public void GetOfferByIdTest()
         {
             int id = 1;
             IHttpActionResult result = _controller.Get(id);
@@ -37,6 +37,17 @@ namespace UnitTestProject2.ControllerTests
             Assert.IsNotNull(contentResult);
             Assert.IsNotNull(contentResult.Content);
             Assert.AreEqual(o.VendorId, contentResult.Content.VendorId);
+        }
+
+        [TestMethod]
+        public void GetUserOffersTest()
+        {
+            IHttpActionResult result = _controller.GetUserOffers(1);
+            var contentResult = result as OkNegotiatedContentResult<IList<Offer>>;
+
+            Assert.IsNotNull(contentResult);
+            Assert.IsNotNull(contentResult.Content);
+            Assert.AreEqual(2, contentResult.Content.Count);
         }
     }
 }
