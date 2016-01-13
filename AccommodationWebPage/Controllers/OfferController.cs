8b59493aecd96c31c1bf9b53218698c885faa72f
jablonskim <matjablonski94@gmail.com>
2016-01-13 17:09:46 +0100

    adding new project

---
+++ AccommodationWebPage/Controllers/OfferController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AccommodationWebPage.Controllers
+{
+    public class OfferController : Controller
+    {
+        // GET: /OfferWorld/ 
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
---
+++ AccommodationWebPage/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AccommodationWebPage.Startup))]
+namespace AccommodationWebPage
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
