cabc929a5d80499c71f58131b8b65898f6e23149
bakala12 <bakala12@o2.pl>
2016-01-17 16:48:46 +0100

    to reset

---
+++ AccommodationWebPage/DataAccess/SearchDataAccessor.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using AccommodationDataAccess.Domain;
+using AccommodationDataAccess.Model;
+using AccommodationDataAccess.Searching;
+using AccommodationShared.Dtos;
+using AccommodationShared.Searching;
+using AccommodationWebPage.Models;
+
+namespace AccommodationWebPage.DataAccess
+{
+    /// <summary>
+    /// Klasa do wyszukiwania ofert.
+    /// </summary>
+    public class SearchDataAccessor
+    {
+        /// <summary>
+        /// Wyszukuje oferty.
+        /// </summary>
+        /// <param name="context">Kontkst bazy danych</param>
+        /// <param name="username">Nazwa użytkwnika</param>
+        /// <param name="criteria">Kryteria wyszukiwania</param>
+        /// <param name="sortBy">Po jakiej właściwości posortować wyniki</param>
+        /// <param name="sortType">Porządek sortowania.</param>
+        /// <returns>Lista z wynikami wyszukiwania.</returns>
+        private IList<OfferViewModel> Search(IAccommodationContext context, string username, 
+            ISearchingCriterion<Offer>[] criteria, SortType sortType, SortBy sortBy)
+        {
+            if (string.IsNullOrEmpty(username)) return null;
+            User u = context.Users.FirstOrDefault(us => us.Username.Equals(username));
+            if (u == null) return null;
+            IQueryable<Offer> offers = context.Offers.Where(o => o.VendorId != u.Id).Where(o => !o.IsBooked);
+            offers = criteria.Aggregate(offers, (current, criterion) => current.Where(criterion.SelectableExpression));
+            offers = offers.Take(20).OrderBy(sortType, sortBy);
+            return offers.ToList().Select(offer => new OfferViewModel(offer)).ToList();
+        }
+
+        public IList<OfferViewModel> SearchByPlace(IAccommodationContext context, PlaceSearchingModel dto)
+        {
+            ISearchingCriterion<Offer> criterion =
+                OffersSearchingCriteriaFactory.CreatePlaceSearchingCriterion(dto.PlaceName, dto.CityName);
+            return Search(context,dto.Username, new[] { criterion }, dto.SortType, dto.SortBy);
+        }
+
+        public IList<OfferViewModel> SearchByDate(IAccommodationContext context,DateSearchingModel dto)
+        {
+            ISearchingCriterion<Offer> criterion =
+                OffersSearchingCriteriaFactory.CreateDateSearchingCriterion(dto.MinimalDate, dto.MaximalDate,
+                    dto.ShowPartiallyMatchingResults);
+            return Search(context,dto.Username, new[] { criterion }, dto.SortType, dto.SortBy);
+        }
+
+        public IList<OfferViewModel> SearchByPrice(IAccommodationContext context,PriceSearchingModel dto)
+        {
+            ISearchingCriterion<Offer> criterion =
+                OffersSearchingCriteriaFactory.CreatePriceSearchingCriterion(dto.MinimalPrice, dto.MaximalPrice);
+            return Search(context,dto.Username, new[] { criterion }, dto.SortType, dto.SortBy);
+        }
+
+        public IList<OfferViewModel> SearchByMultipleCriteria(IAccommodationContext context,AdvancedSearchingModel dto)
+        {
+            ISearchingCriterion<Offer>[] criteria = new[]
+            {
+                OffersSearchingCriteriaFactory.CreatePlaceSearchingCriterion(dto.PlaceName, dto.CityName),
+                OffersSearchingCriteriaFactory.CreateDateSearchingCriterion(dto.MinimalDate, dto.MaximalDate),
+                OffersSearchingCriteriaFactory.CreatePriceSearchingCriterion(dto.MinimalPrice, dto.MaximalPrice)
+            };
+            return Search(context,dto.Username, criteria, dto.SortType, dto.SortBy);
+        }
+
+        public async Task<IList<OfferViewModel>> SearchByPlaceAsync(IAccommodationContext context, PlaceSearchingModel dto)
+        {
+            return await Task.Run(() => SearchByPlace(context, dto));
+        }
+
+        public async Task<IList<OfferViewModel>> SearchByDateAsync(IAccommodationContext context, DateSearchingModel dto)
+        {
+            return await Task.Run(() => SearchByDate(context, dto));
+        }
+
+        public async Task<IList<OfferViewModel>> SearchByPriceAsync(IAccommodationContext context, PriceSearchingModel dto)
+        {
+            return await Task.Run(() => SearchByPrice(context, dto));
+        }
+
+        public async Task<IList<OfferViewModel>> SearchByMultipleCriteriaAsync(IAccommodationContext context,
+            AdvancedSearchingModel dto)
+        {
+            return await Task.Run(() => SearchByMultipleCriteria(context, dto));
+        }
+    }
+}
---
+++ AccommodationWebPage/Models/SearchingModels.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using AccommodationShared.Searching;
+
+namespace AccommodationWebPage.Models
+{
+    /// <summary>
+    /// Model bazowy dla zapytania
+    /// </summary>
+    public abstract class SearchingModel
+    {
+        public string Username { get; set; }
+        public SortType SortType { get; set; }
+        public SortBy SortBy { get; set; }
+        public IList<OfferViewModel> Offers { get; set; } 
+    }
+
+    /// <summary>
+    /// Model dla szukania po miejscu
+    /// </summary>
+    public class PlaceSearchingModel : SearchingModel
+    {
+        public string PlaceName { get; set; }
+        public string CityName { get; set; }
+    }
+
+    /// <summary>
+    /// Model dla szukania po dacie
+    /// </summary>
+    public class DateSearchingModel : SearchingModel
+    {
+
+        public DateTime? MinimalDate { get; set; }
+        public DateTime? MaximalDate { get; set; }
+        public bool ShowPartiallyMatchingResults { get; set; }
+    }
+
+    /// <summary>
+    /// Model dla szukania po cenie
+    /// </summary>
+    public class PriceSearchingModel : SearchingModel
+    {
+        public double? MinimalPrice { get; set; }
+        public double? MaximalPrice { get; set; }
+    }
+
+    /// <summary>
+    /// Model dla szukania zaawansowanego
+    /// </summary>
+    public class AdvancedSearchingModel : SearchingModel
+    {
+        public string PlaceName { get; set; }
+        public string CityName { get; set; }
+        public DateTime? MinimalDate { get; set; }
+        public DateTime? MaximalDate { get; set; }
+        public double? MinimalPrice { get; set; }
+        public double? MaximalPrice { get; set; }
+    }
+}
